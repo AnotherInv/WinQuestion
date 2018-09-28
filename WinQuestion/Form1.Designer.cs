d579f5347c20dff4a62c0159dfe22a02edd39325
Антипов Денис Александрович <eugenemcutalk@gmail.com>
2018-09-28 23:40:14 +0300

    Add project files.

---
+++ WinQuestion/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace WinQuestion
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnYes = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnNo = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnYes
+            // 
+            this.btnYes.Location = new System.Drawing.Point(45, 37);
+            this.btnYes.Name = "btnYes";
+            this.btnYes.Size = new System.Drawing.Size(96, 40);
+            this.btnYes.TabIndex = 0;
+            this.btnYes.Text = "Я кнопочка";
+            this.btnYes.UseVisualStyleBackColor = true;
+            this.btnYes.Click += new System.EventHandler(this.btnYes_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(76, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(197, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Снизу кнопочка и ещё одна кнопочка";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnNo
+            // 
+            this.btnNo.Location = new System.Drawing.Point(190, 37);
+            this.btnNo.Name = "btnNo";
+            this.btnNo.Size = new System.Drawing.Size(96, 40);
+            this.btnNo.TabIndex = 2;
+            this.btnNo.Text = "А я ещё одна кнопочка";
+            this.btnNo.UseVisualStyleBackColor = true;
+            this.btnNo.Click += new System.EventHandler(this.btnNo_Click);
+            this.btnNo.MouseMove += new System.Windows.Forms.MouseEventHandler(this.btnNo_MouseMove);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(89, 92);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(168, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Поздоровайтесь с кнопочками:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(124, 108);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(88, 20);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label3.Enabled = false;
+            this.label3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.label3.Location = new System.Drawing.Point(107, 131);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(116, 15);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Привет, юзерочек <3";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label3.Visible = false;
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(248, 119);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(79, 36);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Сделать как по заданию";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(330, 158);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnNo);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnYes);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Form1";
+            this.Text = "Насущный вопрос";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnYes;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnNo;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ WinQuestion/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinQuestion
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (textBox1.Text == "Привет" || textBox1.Text == "Здарова" || textBox1.Text == "Ку")
+                label3.Visible = true;
+                label3.Enabled = true;
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+            label2.Visible = false;
+            label3.Visible = false;
+            textBox1.Visible = false;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+                btnYes.Text = "Да";
+                btnNo.Text = "Нет";
+                label1.Text = "Вы довольны своей зарплатой?";
+            button3.Enabled = false;
+            button3.Visible = false;
+        }
+
+        private void btnYes_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Я и не сомневался, что ты так думать брат!");
+        }
+
+        private void btnNo_MouseMove(object sender, MouseEventArgs e)
+        {
+            btnNo.Top -= e.Y;
+            btnNo.Left += e.X;
+            if (btnNo.Top < -10 || btnNo.Top > 100)
+                btnNo.Top = 60;
+            if (btnNo.Left < -80 || btnNo.Left > 250)
+                btnNo.Left = 120;
+        }
+
+        private void btnNo_Click(object sender, EventArgs e)
+        {
+            label1.Text = "Попал по кнопочке, умница какой.";
+        }
+    }
+}
